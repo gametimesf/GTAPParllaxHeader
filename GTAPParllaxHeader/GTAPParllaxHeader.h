1e44122ea9d8a2bde9d840695d61672475173133
Mike Silvis <mikesilvis@gmail.com>
2016-11-18 16:47:37 -0800

    Initial Commit

---
+++ GTAPParllaxHeader/GTAPParllaxHeader.h
@@ -0,0 +1,19 @@
+//
+//  GTAPParllaxHeader.h
+//  GTAPParllaxHeader
+//
+//  Created by Mike Silvis on 11/18/16.
+//  Copyright © 2016 Mike Silvis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GTAPParllaxHeader.
+FOUNDATION_EXPORT double GTAPParllaxHeaderVersionNumber;
+
+//! Project version string for GTAPParllaxHeader.
+FOUNDATION_EXPORT const unsigned char GTAPParllaxHeaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GTAPParllaxHeader/PublicHeader.h>
+
+
